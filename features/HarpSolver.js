c2418337e77f7fce7ae22bf01e2bdbe4abe0f671
OmniscientARK <lorenzopiuga@gmail.com>
2023-10-19 17:46:05 -0300

    Initial Commit

---
+++ features/HarpSolver.js
@@ -0,0 +1,33 @@
+import {Feature} from "../util/Feature";
+
+export default class HarpSolver extends Feature {
+    constructor() {
+        super("Harp Solver");
+        this.packetReceived = this.withModifiers(this.packetReceived, {
+            setFilteredClasses: [Java.type("net.minecraft.network.play.server.S2FPacketSetSlot")]
+        })
+        this.guiClosed()
+    }
+
+    packetReceived(packet, event) {
+        let inv = Player.getOpenedInventory();
+        if (inv == null) return
+        if (!inv.toString().includes("Harp - ")) return
+        const item = packet.func_149174_e()
+        const name = item ? item.toString() : ""
+        const slot = packet.func_149173_d()
+        if (name.includes("quartzBlock")) {
+            if (inv.getStackInSlot(slot - 18).getID() === 35) {
+                this.doubleSlot = slot - 9
+            }
+            inv.click(slot, false, "MIDDLE")
+        } else if (this.doubleSlot > -1 && slot === this.doubleSlot) {
+            this.doubleSlot = -1
+            inv.click(slot + 9, false, "MIDDLE")
+        }
+    }
+
+    guiClosed() {
+        this.doubleSlot = -1
+    }
+}
---
+++ util/Feature.js
@@ -0,0 +1,12 @@
+export class Feature {
+    constructor(name) {
+        this.name = name
+        this.id = name.toLowerCase().replace(" ", "_")
+    }
+
+    withModifiers(func, modifiers) {
+        func.modifiers = modifiers || {};
+        return func;
+    }
+}
+
